6f78674c54f974b8869fcf709fed470104ee17e5
LeKileR <63826834+LeKileR@users.noreply.github.com>
2020-06-04 13:13:39 +0200

    Add files via upload

---
+++ radialmenu/config.lua
@@ -0,0 +1,80 @@
+-- Menu configuration, array of menus to display
+menuConfigs = {
+    ['emotes'] = {                                  -- Example menu for emotes when player is on foot
+        enableMenu = function()                     -- Function to enable/disable menu handling
+            local player = GetPlayerPed(-1)
+            return IsPedOnFoot(player)
+        end,
+        data = {                                    -- Data that is passed to Javascript
+            keybind = "PageDown",                         -- Wheel keybind to use (case sensitive, must match entry in keybindControls table)
+            style = {                               -- Wheel style settings
+                sizePx = 600,                       -- Wheel size in pixels
+                slices = {                          -- Slice style settings
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 }
+                },
+                titles = {                          -- Text style settings
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {                              -- Array of wheels to display
+                {
+                    navAngle = 270,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.3,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.6,         -- Maximum radius of wheel in percentage
+                    labels = {"RETOUR", "SITCHAIR", "JOINT", "CLAP", "FOLDARMS", "LEAN"},
+                    commands = {"", "e sitchair", "e joint", "e slowclap", "e foldarms2", "e lean"}
+                },
+                {
+                    navAngle = 285,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.6,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.9,         -- Maximum radius of wheel in percentage
+                    labels = {"SALUTE", "FINGER", "PEACE", "RADIO", "GUARD", "SIT JOINT", "SECU", "GANG1", "GANG2", "COP", "FLIP", "COP2"},
+                    commands = {"e salute", "e finger", "e peace", "e radio", "e guard", "e sit6", "e wait13", "e gangsign", "e gangsign2", "e cop", "e flip", "e cop2"}
+                }
+            }
+        }
+    },
+    ['vehicles'] = {                                -- Example menu for vehicle controls when player is in a vehicle
+        enableMenu = function()                     -- Function to enable/disable menu handling
+            local player = GetPlayerPed(-1)
+            return IsPedInAnyVehicle(player, false)
+        end,
+        data = {                                    -- Data that is passed to Javascript
+            keybind = "PageUp",                         -- Wheel keybind to use (case sensitive, must match entry in keybindControls table)
+            style = {                               -- Wheel style settings
+                sizePx = 400,                       -- Wheel size in pixels
+                slices = {                          -- Slice style settings
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 }
+                },
+                titles = {                          -- Text style settings
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {                              -- Array of wheels to display
+                {
+                    navAngle = 270,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.4,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.9,         -- Maximum radius of wheel in percentage
+                    labels = {"imgsrc:engine.png", "imgsrc:key.png", "imgsrc:doors.png", "imgsrc:trunk.png", "imgsrc:hood.png"},
+                    commands = {"engine", "lock", "rdoors", "trunk", "hood"}
+                }
+            }
+        }
+    }
+}
